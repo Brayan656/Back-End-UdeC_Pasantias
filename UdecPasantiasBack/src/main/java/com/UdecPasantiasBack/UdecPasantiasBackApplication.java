856fa9d9d8ca23d3001ed80149de1a02b7203ef2
unknown <bcardenas656@gmail.com>
2022-05-24 18:44:17 -0500

    arreglo cors

--- UdecPasantiasBack/src/main/java/com/UdecPasantiasBack/UdecPasantiasBackApplication.java
+++ UdecPasantiasBack/src/main/java/com/UdecPasantiasBack/UdecPasantiasBackApplication.java
@@ -2,6 +2,9 @@ package com.UdecPasantiasBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UdecPasantiasBackApplication {
@@ -17,5 +20,13 @@ public class UdecPasantiasBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UdecPasantiasBackApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
